f29e0f917cd1d2d2809f2870d485e6aa16e1cee6
Elaine Froehlich <elaine.froehlich@gmail.com>
2017-11-10 00:54:14 +0000

    Initial Commit

---
+++ object.js
@@ -0,0 +1,10 @@
+var hotel = {
+  name: 'Biltmore',
+  rooms: 500,
+  roomsBooked: 150,
+  roomService: true,
+  pool: false,
+  roomsAvail: function () {
+    return this.rooms - this.roomsBooked;
+  }
+}
